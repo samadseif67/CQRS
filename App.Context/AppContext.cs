9a5d19754634621a834bf9a8c6d14b72d173ef9c
samad <samadseif67@gmail.com>
2024-12-02 10:06:23 +0330

    1403-09-12 1006

---
+++ App.Context/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Context
+{
+    public class AppContext:DbContext
+    {
+        public AppContext(DbContextOptionsBuilder<AppContext> options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
