4dcd7f2748518cf23e4597f6769c5ca45d9bd618
samad <samadseif67@gmail.com>
2024-12-02 09:31:09 +0330

    1403-09-12 0931

---
+++ Entites/BaseEntites.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites
+{
+    public class BaseEntites
+    {
+        public int ID { get; set; }
+        public DateTime CreateDateTime { get; set; }
+
+
+    }
+}
